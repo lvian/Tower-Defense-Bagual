26f130544d1003b09ecc520faae54d0dc314179e
Leandro Vian <leandrovian@hotmail.com>
2013-03-29 00:01:58 -0300

    Added oil quall and metal widow notifications

--- Assets/Script/SceneScripts/FoodFactory.cs
+++ Assets/Script/SceneScripts/FoodFactory.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class FoodFactory : LevelScript {
 	private bool nailNotificationShowed = false;
+	private bool oilNotificationShowed = false;
 	private bool steamNotificationShowed = false;
+	private bool widowNotificationShowed = false;
 	
 	public override void Awake ()
 	{
@@ -12,6 +14,7 @@ public class FoodFactory : LevelScript {
 
 	public override void Start ()
 	{
+		_playerManager.UnlockedTowers = "NailSpitter";
 		base.Start();
 	}
 
@@ -35,7 +38,7 @@ public class FoodFactory : LevelScript {
 			if(GameObject.FindGameObjectsWithTag("Respawn").Length > 0){
 				Notification n = new NotificationWindow(
 				new Rect(0f, 0f, 300f, 200f),
-				"Here they come! The bastards use to load our furnaces, and now they're are trying to destroy our factory. " +
+				"Here they come! We call them the \"Steam Rebels\", these bastards used to load our furnaces, now they're are trying to destroy our factory. " +
 				"But don't worry, they're slow an always in need of maintenance. You should be able to hold them off nice and easy!",
 				null);
 				Messenger<Notification>.Broadcast("SendNotification", n);
@@ -44,10 +47,37 @@ public class FoodFactory : LevelScript {
 		}
 		if(_scriptWaves){
 			if(Application.loadedLevel == 1){
-				if(_scriptWaves.CurrentWave >= 5)
+				if(_scriptWaves.CurrentWave == 5)
+				{
 					_playerManager.UnlockedTowers = "NailSpitter OilSquall";
-				else
-					_playerManager.UnlockedTowers = "NailSpitter";
+					if(!oilNotificationShowed){
+						Notification n = new NotificationWindow(
+							new Rect(0f, 0f, 300f, 200f),
+							"Hey there, good news, I just finished the last ajusts on the new tower." +
+							"I called it the \"Oil Squall\"!! What? Didn't liked? Sorry, no time to change it now." +
+							"This tower wont attack, but the oil it spreads around will slow Sir Misit robots, make sure to have one at choke points!",
+							Resources.Load("Icons/oilSquallIcon") as Texture2D,
+							null);
+							Messenger<Notification>.Broadcast("SendNotification", n);
+						oilNotificationShowed = true;
+					}
+					
+				}
+				if(_scriptWaves.CurrentWave == 6)
+				{
+					if(!widowNotificationShowed){
+						Notification n = new NotificationWindow(
+							new Rect(0f, 0f, 300f, 200f),
+							"Hey there, bad news, I just got warned that Sir Misit is sending a new type of robots against us." +
+							"The \"Black Widow\", as he called, were once used as pipe cleaners, deliverers and even for surveilance." +
+							"That robot frame is weaker than the steam rebel, but faster, also it can lay eggs that spawn smaller spiders.",
+							null);
+							Messenger<Notification>.Broadcast("SendNotification", n);
+							widowNotificationShowed = true;
+					}
+					
+				}
+				
 			}
 		}
 		else{
